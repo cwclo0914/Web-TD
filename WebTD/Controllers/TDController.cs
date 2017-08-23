111f1c3a885dd5e12d33c00e883d7f188ef5b9ff
CHEUK WA CHRISTOPHER LO <christopher.lo@avantcorp.com>
2017-08-23 14:49:08 +0900

    Initial Publish

---
+++ WebTD/Controllers/TDController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class TDController : Controller
+    {
+        // GET: TD
+        public ActionResult Index()
+        {
+            return View(); // cshtml
+            //return File("~/Statics/index.html", "text/html"); // static html
+        }
+    }
+}
