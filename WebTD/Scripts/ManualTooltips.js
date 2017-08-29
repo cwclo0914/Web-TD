64de366fae5ab83d6507f002b1f6dea4cbdf0215
CHEUK WA CHRISTOPHER LO <christopher.lo@avantcorp.com>
2017-08-29 12:13:45 +0900

    Styling

---
+++ WebTD/Scripts/ManualTooltips.js
@@ -0,0 +1,31 @@
+﻿
+// 取扱説明書上における画像のツールチップを設定するJavascriptです
+// Content部分は改行や太字を可能にするScriptです
+// Positionはhtmlの位置によって変更します
+
+$("#stageToolTip").tooltip({
+    content: function () {
+        return this.getAttribute("title");
+    },
+    position: {
+        my: "top", at: "bottom", of: "#stagePic"
+    }
+});
+
+$("#buttonToolTip").tooltip({
+    content: function () {
+        return this.getAttribute("title");
+    },
+    position: {
+        my: "right", at: "left", of: "#buttonPic"
+    }
+});
+
+$("#valueToolTip").tooltip({
+    content: function () {
+        return this.getAttribute("title");
+    },
+    position: {
+        my: "right", at: "left", of: "#valuePic"
+    }
+});
