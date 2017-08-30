51821bcbbd6d6cbaed207d9bbedb25ec0d9a7151
zakisey <yasuhiiro.sezaki@diva.co.jp>
2017-08-30 14:56:28 +0900

    APIを追加

---
+++ WebTD/Models/Record.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Login.Models
+{
+    public class Record
+    {
+        [Key, Column(Order = 0)]
+        public string UserName { get; set; }
+        [Key, Column(Order = 1)]
+        public int StageNum { get; set; }
+        public int Stars { get; set; }
+    }
+}
